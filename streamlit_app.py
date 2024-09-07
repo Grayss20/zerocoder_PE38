1d55fb91fc8311fcd3f3ae9e18f113f7c3c53759
Grayss20 <157813957+Grayss20@users.noreply.github.com>
2024-09-07 18:29:31 +0100

    1

---
+++ streamlit_app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import openai
+import os
+from parse_hh import get_candidate_info, get_job_description
+
+
+def request_gpt(system_prompt, user_prompt):
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt},
+        ],
+        max_tokens=1000,
+        temperature=0.0,
+    )
+
+    return response.choices[0].message.content
+
+
+client = openai.Client(
+    api_key=os.getenv("OPENAI_API_KEY"))
+
+SYSTEM_PROMPT = """
+Проскорь кандидата, насколько он подходит для данной вакансии.
+
+Сначала напиши короткий анализ, который будет пояснять оценку.
+Отдельно оцени качество заполнения резюме (понятно ли, с какими задачами сталкивался кандидат и каким образом их решал?). Эта оценка должна учитываться при выставлении финальной оценки - нам важно нанимать таких кандидатов, которые могут рассказать про свою работу
+Потом представь результат в виде оценки от 1 до 10.
+""".strip()
+
+st.title("CV scoring app")
+
+st.subheader("This app is used to score CVs.")
+
+job_description_url = st.text_area("Enter the job description url")
+cv_url = st.text_area("Enter the CV url")
+
+if st.button("Score CV"):
+    with st.spinner("Scoring CV..."):
+        job_description = get_job_description(job_description_url)
+        cv = get_candidate_info(cv_url)
+        user_prompt = f"Job description: {job_description}\nCV: {cv}"
+        response = request_gpt(SYSTEM_PROMPT, user_prompt)
+    st.write(f'Cv scored: {response}')
